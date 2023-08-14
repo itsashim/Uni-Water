04707a861a7f6e925cac234fd47de8d1c705ad88
itsashim <thesaarvik@gmail.com>
2023-08-14 12:28:05 +0545

    Shrawan-29

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+"use  strict";
+$(document).ready(function () {
+  $(".form_next").on("click", function () {
+    $(".layout_1").toggleClass("hide");
+    $(".layout_2").toggleClass("hide");
+  });
+});
